2590787df19ee5b107c7397c9f1fa6e129693962
deepakgourav <130755380+deepakgourav@users.noreply.github.com>
2025-06-16 22:56:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -130,4 +130,5 @@ def predict_sequence():
 # ---------- Run App ----------
 if __name__ == '__main__':
     initialize_data()
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
